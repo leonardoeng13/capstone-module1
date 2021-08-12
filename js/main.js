4b599c916850a0ff17c2cea16db52c1a2b99930c
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2021-08-12 03:13:48 -0300

    Linters Fix

--- js/main.js
+++ js/main.js
@@ -33,58 +33,58 @@ menuItem5.addEventListener('click', () => {
 });
 
 const guests = [
-    {
-      guest: 'Leo de Andrade',
-      imgUrl: 'images/speakers/Leo.png',
-      companyDes: 'XXXXXXXXX',
-      description: 'C-Level Experience almost 15 years',
-    },
-    {
-      guest: 'Fabia',
-      imgUrl: 'images/speakers/Fabia.jpg',
-      companyDes: 'XXXXXXXX',
-      description: 'LOREN LOREN LOREN LOREN LOREN',
-    },
-    {
-      guest: 'Juliana',
-      imgUrl: 'images/speakers/Juliana.jpg',
-      companyDes: 'XXXXXXXXXXX',
-      description: 'LOREN LOREN LOREN LOREN LOREN',
-    },
-    {
-      guest: 'Alexandre',
-      imgUrl: 'images/speakers/Alexander.jpg',
-      companyDes: 'XXXXXXXXXXX',
-      description: 'LOREN LOREN LOREN LOREN LOREN',
-    },
-    {
-      guest: 'Andrew',
-      imgUrl: 'images/speakers/andrew-bryant.jpg',
-      companyDes: 'XXXXXXXXXXX',
-      description: 'LOREN LOREN LOREN LOREN LOREN',
-    },
-    {
-      guest: 'Emmanuel',
-      imgUrl: 'images/speakers/EMMANUEL-R-GOFFI.jpg',
-      companyDes: 'XXXXXXXXXXX',
-      description: 'LOREN LOREN LOREN LOREN LOREN',
-    },
-  ];
+  {
+    guest: 'Leo de Andrade',
+    imgUrl: 'images/speakers/Leo.png',
+    companyDes: 'XXXXXXXXX',
+    description: 'C-Level Experience almost 15 years',
+  },
+  {
+    guest: 'Fabia',
+    imgUrl: 'images/speakers/Fabia.jpg',
+    companyDes: 'XXXXXXXX',
+    description: 'LOREN LOREN LOREN LOREN LOREN',
+  },
+  {
+    guest: 'Juliana',
+    imgUrl: 'images/speakers/Juliana.jpg',
+    companyDes: 'XXXXXXXXXXX',
+    description: 'LOREN LOREN LOREN LOREN LOREN',
+  },
+  {
+    guest: 'Alexandre',
+    imgUrl: 'images/speakers/Alexander.jpg',
+    companyDes: 'XXXXXXXXXXX',
+    description: 'LOREN LOREN LOREN LOREN LOREN',
+  },
+  {
+    guest: 'Andrew',
+    imgUrl: 'images/speakers/andrew-bryant.jpg',
+    companyDes: 'XXXXXXXXXXX',
+    description: 'LOREN LOREN LOREN LOREN LOREN',
+  },
+  {
+    guest: 'Emmanuel',
+    imgUrl: 'images/speakers/EMMANUEL-R-GOFFI.jpg',
+    companyDes: 'XXXXXXXXXXX',
+    description: 'LOREN LOREN LOREN LOREN LOREN',
+  },
+];
 
-  function createProjectsSeciton() {
-    if (guests.length > 0) {
-      const guestsSection = document.querySelector('.guestsSection');
-  
-      for (let i = 0; i < guests.length; i += 1) {
-        guestsSection.innerHTML += '<li class="guest-container">'
-          + `<img src="${guests[i].imgUrl}" alt="Guest logo"/>`
-          + '<div class="guest">'
-          + `<h2>${guests[i].guest}</h2>`
-          + `<h3>${guests[i].companyDes}</h2>`
-          + `<p>${guests[i].description}</p>`
-          + '</div>'
-          + '</li>';
-      }
+function createProjectsSeciton() {
+  if (guests.length > 0) {
+    const guestsSection = document.querySelector('.guestsSection');
+
+    for (let i = 0; i < guests.length; i += 1) {
+      guestsSection.innerHTML += '<li class="guest-container">'
+        + `<img src="${guests[i].imgUrl}" alt="Guest logo"/>`
+        + '<div class="guest">'
+        + `<h2>${guests[i].guest}</h2>`
+        + `<h3>${guests[i].companyDes}</h2>`
+        + `<p>${guests[i].description}</p>`
+        + '</div>'
+        + '</li>';
     }
   }
-  createProjectsSeciton();
+}
+createProjectsSeciton();
